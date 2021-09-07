b8a29a01b62618f7af7131300143fe37788b6c07
Andrew Dunn <1995ajld@gmail.com>
2021-09-06 22:28:26 -0400

    Added Cycle Counter, fixed Kotlin main function

--- src/main/kotlin/NESMain.kt
+++ src/main/kotlin/NESMain.kt
@@ -1,15 +1,28 @@
 import emulator.hardware.CPU
 import emulator.opCodes.OpCodeDecoder
 
+fun main(args: Array<String>) {
+    val nesMain = NESMain()
+    while(true){
+        nesMain.cycle()
+        nesMain.drawFrame()
+    }
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
 class NESMain {
     val cpu = CPU()
     val decoder = OpCodeDecoder(cpu)
 
-    fun main() {
-
+    fun cycle(){
+        while(cpu.cycles <= 27756){
+            cpu.opCode = cpu.ram[cpu.programCounterRegister.toInt()]
+            cpu.programCounterRegister = (cpu.programCounterRegister + 1u).toUShort()
+            decoder.decodeOpCode()
+        }
     }
 
-    fun cycle(){
+    fun drawFrame(){
 
     }
 
